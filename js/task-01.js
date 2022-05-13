5af16bb37609472e0c3a9fb9683792e12cad8b50
Renat <72465313+teenrflagrage@users.noreply.github.com>
2022-05-13 14:21:57 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,31 @@
 const categoriesList = document.querySelector("#categories");
 console.log(`Number of categories: ${categoriesList.childElementCount}`);
-console.log(categoriesList.children[0].textContent);
-console.log(categoriesList.children.length);
+// console.log(categoriesList.children[0]);
 
-const items = document.querySelector(".item > h2");
-console.log(`Category: ${items.textContent}`);
+const categoriesFn = (e) => {
+    return [...e].map(el => el );
+}
+// 1
+const items = document.querySelectorAll("h2");
+const categoryEl = categoriesFn(items);
+console.log(`Category: ${categoryEl[0].textContent}`);
 
+const itemsEl = document.querySelectorAll(".item > ul");
+console.log(`Elements: ${itemsEl[0].children.length}`);
+// 2
+console.log(`Category: ${categoryEl[1].textContent}`);
+console.log(`Elements: ${itemsEl[1].children.length}`);
+// 3
+console.log(`Category: ${categoryEl[2].textContent}`);
+console.log(`Elements: ${itemsEl[2].children.length}`);
 
-const itemsEl = document.querySelector(".item > ul");
-console.log(`Elements: ${itemsEl.childElementCount}`);
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
